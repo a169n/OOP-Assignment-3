6c36962e3ad42f427be21acb85f0e80436d15ad4
Дарина <darina.bakeeva2005@gmail.com>
2023-02-02 23:08:18 +0600

    making changes in Login class

--- src/Login.java
+++ src/Login.java
@@ -1,3 +1,16 @@
-public class Login {
+import java.util.Scanner;
 
+public class Login extends Registration implements Password {
+    DB_functions db= new DB_functions();
+
+    @Override
+    public boolean checkPasswordValidity(String password){
+        //some statements
+        return true;
+    }
+    public void login(){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter username");
+
+    }
 }
