444e699d03e353d548020c61f2b601ecca76165a
Дарина <darina.bakeeva2005@gmail.com>
2023-02-03 12:41:25 +0600

    added Tasks class

---
+++ src/Tasks.java
@@ -0,0 +1,22 @@
+public class Tasks {
+    public void addTask(){
+
+    }
+
+
+    public void editTask(){
+
+    }
+
+    public void ReadTasks(){
+
+    }
+
+    public void deleteTasks(){
+
+    }
+
+    public void sortByDeadline(){
+
+    }
+}
