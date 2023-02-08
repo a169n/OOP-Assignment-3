e873adfc4288eb569a0488d6458c3d30de9e3308
Ozzy <121328432+Ozzy0153@users.noreply.github.com>
2023-02-08 11:38:50 +0600

    need to be finished

---
+++ src/Student.java
@@ -0,0 +1,5 @@
+public class Student extends user{
+    public Student(String username, String password, String position){
+        super(username, password, position);
+    }
+}
--- src/Tasks.java
+++ src/Tasks.java
@@ -11,14 +11,7 @@ public class Tasks {
     }
 
     public void addTask(String task_name) {
-        Statement statement= null;
-        try{
-            String query = String.format("insert into %s(task_name) values('%s');", "tasks", task_name);
-            statement.executeUpdate(query);
-            System.out.println("Task added.");
-        }catch (Exception e) {
-            System.out.println(e);
-        }
+
     }
 
     public void editTask() {
