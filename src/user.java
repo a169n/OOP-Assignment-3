7205984593f2f7879e30502be5bcdbdec6011de0
Aibyn Talgatov <110616859+a169n@users.noreply.github.com>
2023-02-09 14:07:06 +0600

    :)

--- src/user.java
+++ src/user.java
@@ -1,62 +1,34 @@
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+public class User {
+    String password, username, position;
+    public User(){
 
-public abstract class user {
-    private String username;
-    private String position;
-    private String password;
-
-    static Connection connection = null;
-    PreparedStatement ps =null;
-
-    static ResultSet rs = null;
-
-    public user(){
     }
-    public user(String username, String position, String password){
-        this.username = username;
-        this.password = password;
-        this.position = position;
+    public User(String username, String password, String position){
+        setPassword(password);
+        setPosition(position);
+        setUsername(username);
     }
-
-    public void setUsername(String name) {
-        this.username = name;
+    public String getPassword() {
+        return password;
     }
-
 
     public void setPosition(String position) {
         this.position = position;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public void setPassword(String password) {
         this.password = password;
     }
 
-    public String getUsername() {
-        return username;
-    }
-
-
     public String getPosition() {
         return position;
     }
 
-    public String getPassword() {
-        return password;
-    }
-    public void insert() throws SQLException {
-        new DB_methods();
-        ps = connection.prepareStatement("INSERT INTO users (username, password, role) values (?, ? ,?)");
-        ps.setString(1, getUsername());
-        ps.setString(2, getPassword());
-        ps.setString(3, getPosition());
-        ps.execute();
-    }
-
-    @Override
-    public String toString() {
-        return ("Username: " + getUsername() + " Possition: " + getPosition());
+    public String getUsername() {
+        return username;
     }
 }
