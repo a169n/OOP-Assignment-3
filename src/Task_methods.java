d011f2e41c908a6dec5ba1652b17fcbc8816e5d3
Aibyn Talgatov <110616859+a169n@users.noreply.github.com>
2023-02-09 21:50:29 +0600

    now Task_methods extends DB_methods

--- src/Task_methods.java
+++ src/Task_methods.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Task_methods {
+public class Task_methods extends DB_methods {
     DB_methods db = new DB_methods();
     Scanner sc = new Scanner(System.in);
 
@@ -8,7 +8,7 @@ public class Task_methods {
     public void task_add_new(){
         System.out.print("Task name: ");
         String task = sc.nextLine();
-        db.insertTask(task, db.getID(db.username));
+        insertTask(task, getID(username));
     }
 
     public void task_update(){
@@ -19,16 +19,16 @@ public class Task_methods {
         System.out.print("New task name: ");
         String new_task = sc.nextLine();
 
-        db.update_the_task(Integer.parseInt(task_ID), new_task);
+        update_the_task(Integer.parseInt(task_ID), new_task);
     }
     public void task_delete(){
         System.out.print("Task name that will be deleted: ");
         //check if such task exists
         String task = sc.nextLine();
 
-        db.delete_task_by_name(task);
+        delete_task_by_name(task);
     }
     public void task_read(){
-        db.output_tasks(db.getID(db.getUsername()));
+        output_tasks(getID(getUsername()));
     }
 }
